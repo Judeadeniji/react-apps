3043214d7e6c15d54fee71834e5f211e692056c0
Judeadeniji <judeadeniji66@gmail.com>
2023-07-13 12:02:11 +0100

    ...

---
+++ nextjs-app/src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl">
+      My first next app
+    </h1>
+    </main>
+  )
+}
